e95fa168122c4ab808b2e0528a25d67fd8ea4067
B4shCat <mcartier445@gmail.com>
2023-11-07 21:48:24 -0500

    refactored code into methods

--- sockserver.py
+++ sockserver.py
@@ -1,14 +1,14 @@
 import socket
 
-# set host IP and port
+def listen_handler():
+    sock.bind((host_ip, host_port))
+    print('[+] Awaiting connection from client...')
+    sock.listen()
+    remote_target, remote_ip = sock.accept()
+    print(f'[+] Connection received from {remote_ip[0]}')
+    remote_target.close()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_ip = '127.0.0.1'
 host_port = 2222
-
-# Create socket object for IPv4 connection
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((host_ip, host_port))
-print('[+] Awaiting connection from client...')
-sock.listen()
-remote_target, remote_ip = sock.accept()
-print(f'[+] Connection received from {remote_ip}')
-remote_target.close()
+listen_handler()
